f55a1a85eb861e485808d664313b84f31865cbae
ansen <anhui@365yf.com>
2017-06-09 15:37:25 +0800

    更新

--- app/src/main/java/com/ansen/okhttp/MainActivity.java
+++ app/src/main/java/com/ansen/okhttp/MainActivity.java
@@ -22,7 +22,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private OkHttpClient client = new OkHttpClient();
+    private OkHttpClient client = new OkHttpClient();//创建okHttpClient对象
     private TextView tvResult;
 
     private Handler handler=new Handler(){
@@ -48,22 +48,35 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             if(view.getId()==R.id.btn_get){
-                getUserInfo("http://139.196.35.30:8080/OkHttpTest/getUserInfo.do");
+                getUserInfo();
             }else if(view.getId()==R.id.btn_post){
-                login("http://139.196.35.30:8080/OkHttpTest/login.do");
+                login();
             }else if(view.getId()==R.id.btn_upload_file){//上传文件
-                postFile("http://139.196.35.30:8080/OkHttpTest/uploadFile.do");
+                uploadFile();
             }
         }
     };
 
-    private void postFile(String url){
+    private void uploadFile(){
         MultipartBody.Builder builder = new MultipartBody.Builder();
-        builder.addFormDataPart("username", "ansen");
-        builder.addFormDataPart("password", "123456");
+        builder.addFormDataPart("username", "ansen");//表单参数
+        builder.addFormDataPart("password", "123456");//表单参数
 
         builder.setType(MultipartBody.FORM);
         MediaType mediaType = MediaType.parse("application/octet-stream");
+
+        byte[] bytes=getUploadFileBytes();//获取文件内容存入byte数组
+        //上传文件 参数1:name 参数2:文件名称 参数3:文件byte数组
+        builder.addFormDataPart("upload_file", "ansen.txt",RequestBody.create(mediaType,bytes));
+        RequestBody requestBody = builder.build();
+        Request.Builder requestBuider = new Request.Builder();
+        requestBuider.url("http://139.196.35.30:8080/OkHttpTest/uploadFile.do");
+        requestBuider.post(requestBody);
+        execute(requestBuider);
+    }
+
+
+    private byte[] getUploadFileBytes(){
         byte[] bytes=null;
         try {
             InputStream inputStream = getAssets().open("ansen.txt");
@@ -74,35 +87,33 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        builder.addFormDataPart("upload_file", "ansen.txt",RequestBody.create(mediaType,bytes));
-        RequestBody requestBody = builder.build();
-        Request.Builder requestBuider = new Request.Builder();
-        requestBuider.url(url);
-        requestBuider.post(requestBody);
-        execute(requestBuider);
+        return bytes;
     }
 
-    private void getUserInfo(String url){
-        Request.Builder builder = new Request.Builder().url(url);
+    private void getUserInfo(){
+        //创建一个Request
+        Request.Builder builder = new Request.Builder().url("http://139.196.35.30:8080/OkHttpTest/getUserInfo.do");
         execute(builder);
     }
 
-    private void login(String url){
+    private void login(){
+        //把请求参数封装到RequestBody里面
         FormBody.Builder formBuilder = new FormBody.Builder();
-        formBuilder.add("username","ansen");
-        formBuilder.add("password","123");
+        formBuilder.add("username","ansen");//请求参数一
+        formBuilder.add("password","123");//请求参数二
         RequestBody requestBody = formBuilder.build();
 
-        Request.Builder builder = new Request.Builder().url(url).post(requestBody);
+        Request.Builder builder = new Request.Builder().url("http://139.196.35.30:8080/OkHttpTest/login.do").post(requestBody);
         execute(builder);
     }
 
+    //执行请求
     private void execute(Request.Builder builder){
         Call call = client.newCall(builder.build());
-        call.enqueue(callback);
+        call.enqueue(callback);//加入调度队列
     }
 
+    //请求回调
     private Callback callback=new Callback(){
         @Override
         public void onFailure(Call call, IOException e) {
@@ -112,9 +123,11 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onResponse(Call call, Response response) throws IOException {
+            //从response从获取服务器返回的数据，转成字符串处理
             String str = new String(response.body().bytes(),"utf-8");
             Log.i("MainActivity","onResponse:"+str);
 
+            //通过handler更新UI
             Message message=handler.obtainMessage();
             message.obj=str;
             message.sendToTarget();
